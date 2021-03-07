eb99d2813733744861c10b9b05f53234e901bd23
Mahmud Ahmad <duelwith_mahmud@live.com>
2021-03-07 04:20:22 +0000

    write to single output file, switched to double precision
    todo:
    determine when to stop,
      i.e. on collision
        P.E approx 0,
        Position approx 0

--- calculate_velocity/main.f90
+++ calculate_velocity/main.f90
@@ -10,82 +10,80 @@
 !! @param[in] acceleration mm/s/s
 !------------------------------------------------------------------------------
 function apply_linear_acceleration(time_delta, velocity, acceleration) result(r)
-    integer, intent(in) :: time_delta   ! input, ms
-    integer, intent(in) :: velocity     ! input, mm/s
-    integer, intent(in) :: acceleration ! input, mm/s/s
-    integer             :: r            ! output, mm/s
+    real, intent(in)    :: time_delta   ! input, s
+    real, intent(in)    :: velocity     ! input, m/s
+    real, intent(in)    :: acceleration ! input, m/s/s
+    real                :: r            ! output, m/s
     ! velocity = initial_velocty + (dt * acceleration)
-    r = (time_delta * (acceleration/1000)) + velocity
+    r = (time_delta * acceleration) + velocity
 end function
 
 function calculate_position(time_delta, initial_position, velocity) result(r)
-    integer, intent(in) :: time_delta       ! input, ms
-    integer, intent(in) :: initial_position ! input, mm
-    integer, intent(in) :: velocity         ! input, mm/s
-    integer             :: r                ! output, mm
+    real, intent(in)    :: time_delta       ! input, s
+    real, intent(in)    :: initial_position ! input, m
+    real, intent(in)    :: velocity         ! input, m/s
+    real                :: r                ! output, m
     ! position = (velocity * dt) + intitial_position
-    r = ((velocity/1000) * time_delta) + initial_position
+    r = (velocity * time_delta) + initial_position
 end function
 
 function calculate_potential_energy(mass, position, acceleration_to_rest) result(r)
-    integer, intent(in) :: mass                 ! input, mg
-    integer, intent(in) :: position             ! input, mm (?, I think, haven't actually thought about this one)
-    integer, intent(in) :: acceleration_to_rest ! input, mm/s/s
-    integer             :: r                    ! output, newtons
+    integer, intent(in) :: mass                 ! input, kg
+    real, intent(in)    :: position             ! input, m
+    real, intent(in)    :: acceleration_to_rest ! input, m/s/s
+    real                :: r                    ! output, newtons
     ! potential_energy = mass * acceleration_to_rest * distance_from_rest
-    r = mass * (acceleration_to_rest/1000) * position
+    r = mass * acceleration_to_rest * position
 end function
 
 function calculate_kinetic_energy(mass, velocity) result(r)
-    integer, intent(in) :: mass     ! input, mg
-    integer, intent(in) :: velocity ! input, mm/s
-    integer             :: r        ! output, newtons
+    integer, intent(in) :: mass     ! input, kg
+    real, intent(in)    :: velocity ! input, m/s
+    real                :: r        ! output, newtons
     ! kinetic_energy = (1/2) mass * (velocity * velocity)
-    ! for now, we will assume (1/2) ≈ 1,
-    ! when we support double precision we will worry about this
-    r = (2/2) * mass * ((velocity/1000) ** 2)
+    r = (mass * (velocity ** 2))/2
 end function
 
 function calculate_total_energy(potential_energy, kinetic_energy) result(r)
-    integer, intent(in) :: potential_energy ! input, newtons
-    integer, intent(in) :: kinetic_energy   ! input, newtons
-    integer             :: r                ! output, newtons
+    real, intent(in)    :: potential_energy    ! input, newtons
+    real, intent(in)    :: kinetic_energy      ! input, newtons
+    real                :: r                   ! output, newtons
     r = potential_energy + kinetic_energy
  end function
 
 program calculate_velocity
     implicit none
-    integer :: apply_linear_acceleration
-    integer :: calculate_position
-    integer :: calculate_potential_energy
-    integer :: calculate_kinetic_energy
-    integer :: calculate_total_energy
+    real :: apply_linear_acceleration
+    real :: calculate_position
+    real :: calculate_potential_energy
+    real :: calculate_kinetic_energy
+    real :: calculate_total_energy
 
-    integer :: ACCELERATION     = -9810 ! mm/s/s
-    integer :: mass             = 1000  ! mg
-    integer :: initial_velocty  = 10000 ! mm/s
-    integer :: current_velocity = 10000 ! mm/s
-    integer :: initial_position = 200   ! mm (?, I think, haven't actually thought about this one)
-    integer :: current_position = 0     ! mm (?, I think, haven't actually thought about this one)
-    integer :: time             = 0     ! ms
-    integer :: potential_energy = 0     ! newtons
-    integer :: kinetic_energy   = 0     ! newtons
-    integer :: total_energy     = 0     ! newtons
+    real    :: ACCELERATION     = -9.81 ! m/s/s
+    real    :: DT               = 0.01  ! s
+    integer :: mass             = 1     ! kg
+    real    :: initial_velocty  = 10    ! m/s
+    real    :: current_velocity = 10    ! m/s
+    real    :: initial_position = 2     ! m
+    real    :: current_position = 2     ! m
+    integer :: time             = 0     ! s
+    real    :: potential_energy = 0     ! newtons
+    real    :: kinetic_energy   = 0     ! newtons
+    real    :: total_energy     = 0     ! newtons
 
     potential_energy = calculate_potential_energy(mass, initial_position, -ACCELERATION)
     kinetic_energy   = calculate_kinetic_energy(mass, initial_velocty)
     total_energy     = calculate_total_energy(potential_energy, kinetic_energy)
 
-    open (9, file = "velocity.output", status = "new")
-    open (10, file = "position.output", status = "new")
-    write(9, '(A)') "time, velocity"
-    write(10, '(A)') "time, position"
-    write(9, '(I0, A, I0)') 0, ", ", initial_velocty
-    write(10, '(I0, A, I0)') 0, ", ", initial_position
+    open (9,  file = "output.csv", status = "new")
+    ! write(9,  '(A)') "time, velocity"
+    write(9, *) "time, acceleration, velocity, position, potential_energy, kinetic_energy, total_energy"
+    write(9, *) 0, ", ", 0, ", ", initial_velocty , ", ", current_velocity, ", ", &
+                potential_energy, ", ", kinetic_energy, ", ", total_energy
 
-    do while (total_energy > 0 .OR. time .EQ. 0)
-        current_velocity = apply_linear_acceleration(1, current_velocity, ACCELERATION)
-        current_position = calculate_position(1, current_position, current_velocity)
+    do while (total_energy > 0.001 .OR. time .EQ. 0)
+        current_velocity = apply_linear_acceleration(DT, current_velocity, ACCELERATION)
+        current_position = calculate_position(DT, current_position, current_velocity)
         potential_energy = calculate_potential_energy(mass, current_position, -ACCELERATION)
         kinetic_energy   = calculate_kinetic_energy(mass, current_velocity)
         total_energy     = calculate_total_energy(potential_energy, kinetic_energy)
@@ -96,11 +94,12 @@ program calculate_velocity
                 " current_potential_energy: ", potential_energy, " current_kinetic_energy: ", kinetic_energy,     &
                 " total_energy: ", total_energy
 
-        write(9,'(I0, A, I0)') time, ", ", current_velocity
-        write(10,'(I0, A, I0)') time, ", ", current_position
+        ! write(9,  '(F0.0, A, F0.0)') time, ", ", current_velocity
+        write(9, *) time, ", ", ACCELERATION, ", ", current_velocity, ", ", current_position, ", ",               &
+                    potential_energy, ", ", kinetic_energy, ", ", total_energy
+
     end do
 
     close(9)
-    close(10)
 
 end program calculate_velocity
